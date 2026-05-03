a9b932b1fe90c0ce4bfbcb33ee2b6dc6cc0cde5f
savchenko2277 <jokarikop@gmail.com>
2026-05-03 05:30:41 +0400

    UPD

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,11 +1,14 @@
 import Swiper from "swiper";
-import { Navigation, EffectFade } from "swiper/modules";
+import { Navigation, EffectFade, Autoplay } from "swiper/modules";
 
 (() => {
 
     const swiper = new Swiper('.hero__swiper', {
-        modules: [Navigation, EffectFade],
+        modules: [Navigation, EffectFade, Autoplay],
         effect: 'fade',
+        autoplay: {
+            delay: 5000,
+        },
         fadeEffect: { crossFade: true },
         navigation: {
             nextEl: document.querySelector('.hero__navigation .custom-navigation__btn_next'),
